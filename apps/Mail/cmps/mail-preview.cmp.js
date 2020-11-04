92d9b9bdb993a90d757b407aa0b3bbf5126dedb2
Nehoray Ilani <neho12@gmail.com>
2020-11-04 17:47:04 +0200

    added expd preview mail list

--- apps/Mail/cmps/mail-preview.cmp.js
+++ apps/Mail/cmps/mail-preview.cmp.js
@@ -2,32 +2,51 @@
 export default {
     props: ['mail'],
     template: `
-        <section class="mail-preview">
+        <section class="mail-preview" @click="isShowExpndMail=!isShowExpndMail">
         <li>
             <div class="mail-prv-star"><i class="far fa-star"></i></div>
-            <div class="mail-prv-sender">{{ mail.sender }}</div>
+            <div class="mail-prv-sender">{{ mail.senderName }}</div>
             <div class="mail-prv-subject">{{ mail.subject }}</div>
             <div class="mail-prv-body">{{ mail.body }}</div>
             <div class="mail-prv-status">{{ mail.isRead }}</div>
             <div class="mail-prv-date">{{ getDate }}</div>
         </li>
+        <div v-if="isShowExpndMail">
+            <div class="exp-mail-prv">
+                <div class="exp-mail-header">
+                    <div class="mail-expd-subject">{{ mail.subject }}</div>
+                    <div class="open-actions-mail-exp">
+                    <i class="fas fa-ellipsis-h"></i><i class="fas fa-expand-alt"></i>
+                    </div>
+                    </div>
+                                    <div class="exp-mail-sub-header">
+                        <div class="mail-expd-sender-name">{{ mail.senderName }}</div>
+                        <div class="mail-expd-sender-email"><{{ mail.senderEmail }}></div>
+                    </div>
+                    <div class="mail-expd-body">{{ mail.body }}</div>
+                </div>
+            </div>
+        </div>
         </section>
     `,
     data() {
         return {
-            currMails: null
+            currMails: null,
+            isShowExpndMail: false,
         }
     },
     computed: {
         getDate() {
             var nowTime = new Date().getTime()
             var options;
-            if  (Math.floor((nowTime - this.mail.sentAt)/60000) < 60*24 ){
+            if  (Math.floor((nowTime - this.mail.sentAt)/60000) < 59*24 ){
+                return new Date(this.mail.sentAt).toLocaleTimeString('en-IL', { hour: '2-digit', minute: '2-digit' })
+            } else if(Math.floor((nowTime - this.mail.sentAt)/60000) < 60*24 ){
                 options = { month: 'long', day: 'numeric' };
             } else {
                 options = { year: 'numeric', month: 'numeric', day: 'numeric' };
             }
-            var date = new Date(this.mail.sentAt).toLocaleDateString('en-US', options)
+            var date = new Date(this.mail.sentAt).toLocaleDateString('en-IL', options)
             return date
         }
     },
--- apps/Mail/services/mail-service.js
+++ apps/Mail/services/mail-service.js
@@ -3,9 +3,9 @@ import { utilService } from '../../../services/util-service.js'
 const MAILS_STORAGE_KEY = 'mails_db'
 
 var gMails = [
-    {id: '35254r1d1', sender: 'Ido', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt : 1604496265865},
-    {id: '643fhdh64', sender: 'Roee', subject: 'Some Nice Offer!', body: 'Pick up!', isRead: false, sentAt : 1551133930594},
-    {id: '6436gddfd', sender: 'Neho', subject: 'How are you buddy?', body: 'Pick up!', isRead: false, sentAt : 1551133930594}
+    {senderEmail: 'idogte2@gmail.com', id: '35254r1d1', senderName: 'Ido', subject: 'Wassap?', body: 'Hi, I would be happy to get started...', isRead: false, sentAt : 1604496265865},
+    {senderEmail: 'roeeg54e@gmail.com', id: '643fhdh64', senderName: 'Roee', subject: 'Some Nice Offer!', body: 'This seems like a great video clip...', isRead: false, sentAt : 1551133930594},
+    {senderEmail: 'neho326@gmail.com', id: '6436gddfd', senderName: 'Neho', subject: 'How are you buddy?', body: 'Pick up!', isRead: false, sentAt : 1551133930594}
 ]
 export const mailService = {
     getMails
