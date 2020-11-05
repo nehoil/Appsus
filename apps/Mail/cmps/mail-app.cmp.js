612274d47be84f8fb4608aaca34793d084b9fbba
Nehoray Ilani <neho12@gmail.com>
2020-11-05 13:57:18 +0200

    added compose feature

--- apps/Mail/cmps/mail-app.cmp.js
+++ apps/Mail/cmps/mail-app.cmp.js
@@ -4,14 +4,45 @@ import mailSideMenu from './mail-side-menu.cmp.js';
 
 
 export default {
+    name: 'mailApp',
     template: `
         <section class="mail-app">
+            <div class="compose-container compose-container-w" v-if="isShowCompose">
+                <form @submit.prevent="addMail">
+                <div class="compose-title">
+                    New Message
+                </div>
+                <div class="compose-recipients">
+                    <input v-model="newMail.sentEmail" type="text" placeholder="To"  ref="toInput" required>
+                </div>
+                <div class="compose-subject">
+                    <input v-model="newMail.subject" type="text" placeholder="Subject" required>
+                </div>
+                <div class="compose-cc">
+                    <input type="text" placeholder="Cc">
+                </div>
+                <div class="compose-bcc">
+                    <input type="text" placeholder="Bcc">
+                </div>
+                <div class="compose-body">
+                    <textarea v-model="newMail.body" cols="30" rows="10" placeholder="Your Message" required></textarea>
+                </div>
+                <div class="compose-footer">
+                    <div class="compose-send">
+                        <button>Send</button>
+                    </div>
+                    <div class="compose-remove">
+                        <i class="fas fa-trash" aria-hidden="true"></i>
+                    </div>
+                </div>
+                </form>
+            </div>
             <div class="mail-app-main-content-container">
                 <div class="mail-side-menu">
-                    <mail-side-menu :mails="mails" />
+                    <mail-side-menu :mails="mails" @doFilter="setFilter" @doCompose="showCompose"/>
                 </div>
                 <div class="mail-list-container">
-                    <mail-list :mails="mails" />
+                    <mail-list :mails="mailsToShow" />
                 </div>
             </div>
         </section>
@@ -19,15 +50,36 @@ export default {
     data() {
         return {
             mails: '',
+            isShowCompose: true,
+            filterBy: null,
+            newMail: { sentEmail: null, isNote: false, isDraft: false, isStar: false, senderEmail: 'myname@gmail.com', senderName: 'me', subject: '', body: '', isRead: false, sentAt: null }
         }
     },
     computed: {
+        mailsToShow() {
+            if (!this.filterBy) return this.mails;
+            return this.mails.filter(mail => mail[this.filterBy])
+        }
     },
     methods: {
+        setFilter(filter) {
+            this.filterBy = filter;
+        },
+        addMail() {
+            this.isShowCompose = false;
+            this.newMail.sentAt = new Date().getTime();
+            const mailDeepCopy = JSON.parse(JSON.stringify(this.newMail));
+            mailService.addMail(mailDeepCopy);
+            this.newMail = mailService.getEmptyMail()
+        },
+        showCompose() {
+            this.isShowCompose = true;
+            setTimeout(() => this.$refs.toInput.focus(), 100)
+        }
     },
     created() {
         mailService.getMails()
-        .then(mails => this.mails = mails)
+            .then(mails => this.mails = mails)
     },
     components: {
         mailList,
--- apps/Mail/cmps/mail-preview.cmp.js
+++ apps/Mail/cmps/mail-preview.cmp.js
@@ -7,7 +7,7 @@ export default {
     template: `
         <section class="mail-preview">
         <li @click="mailClicked" :class="{ bold: !mail.isRead }">
-            <div class="mail-prv-star"><i class="far fa-star"></i></div>
+            <div class="mail-prv-star" @click.stop="starMail"><i :class="starClass"></i></div>
             <div class="mail-prv-sender">{{ mail.senderName }}</div>
             <div class="mail-prv-subject">{{ mail.subject }}</div>
             <div class="mail-prv-body">{{ mail.body }}</div>
@@ -18,7 +18,7 @@ export default {
                 <div class="exp-mail-header">
                     <div class="mail-expd-subject">{{ mail.subject }}</div>
                     <div class="open-actions-mail-exp" @click="isShowMenu = !isShowMenu"><i class="fas fa-ellipsis-h"></i>
-                        <mail-action-menu v-if="isShowMenu" :mail="mail" @menuClicked="toggleMenu" />
+                        <mail-action-menu v-if="isShowMenu" :mail="mail" @menuClicked="this.isShowMenu = !this.isShowMenu" @unMarked="isShowExpndMail = false" />
                     <router-link :to="'/mail/' + mail.id" exact>
                     <i class="fas fa-expand-alt"></i>
                     </router-link>
@@ -41,13 +41,17 @@ export default {
         }
     },
     computed: {
+        starClass(){
+            if (this.mail.isStar) return 'filter-icon fas fa-star starred'
+            return 'far fa-star'
+        },
         isRead(){
             if (!this.mail.isRead) return 'bold';
         },
         getDate() {
             var nowTime = new Date().getTime()
             var options;
-            if  (Math.floor((nowTime - this.mail.sentAt)/60000) < 59*24 ){
+            if  (Math.floor((nowTime - this.mail.sentAt)/60000) < 60*12 ){
                 return new Date(this.mail.sentAt).toLocaleTimeString('en-IL', { hour: '2-digit', minute: '2-digit' })
             } else if(Math.floor((nowTime - this.mail.sentAt)/60000) < 60*24 ){
                 options = { month: 'long', day: 'numeric' };
@@ -60,13 +64,15 @@ export default {
     },
     methods: {
         toggleMenu(){
-            console.log('works');
             this.isShowMenu = !this.isShowMenu
         },
         mailClicked(){
             this.isShowExpndMail=!this.isShowExpndMail
             mailService.unMarkMail(this.mail)
         },
+        starMail(){
+            mailService.starMail(this.mail)
+        }
     },
     created() {
     },
